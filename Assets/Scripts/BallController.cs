7e81626363b2a1a59a17ce48b7fac154d992c41f
Cooper Ross, UBCO <c.mross2907@gmail.com>
2025-01-28 13:27:42 -0800

    Finish
    + Added Walls
    + Added Materials
    + Added InputHandler
    - Fixed movement to match SRP

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,30 +1,13 @@
 using UnityEngine;
 
+
 public class BallController : MonoBehaviour{
 
-    public Rigidbody sphereRigidBody;
-    public float ballSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start(){
-        Debug.Log("Calling the start method");
-    }
-
-    // Update is called once per frame
-    void Update(){
-        Vector2 inputVector = Vector2.zero;
-        if(Input.GetKey(KeyCode.W)){
-            inputVector += Vector2.up;
-        }
-        if(Input.GetKey(KeyCode.S)){
-            inputVector += Vector2.down;
-        }
-        if(Input.GetKey(KeyCode.D)){
-            inputVector += Vector2.right;
-        }
-        if(Input.GetKey(KeyCode.A)){
-            inputVector += Vector2.left;
-        }
-        Vector3 inputXZPlane = new Vector3(inputVector.x,0,inputVector.y);
+    [SerializeField] private Rigidbody sphereRigidBody;
+    [SerializeField] float ballSpeed;
+    public void MoveBall(Vector2 input){
+        Vector3 inputXZPlane = new Vector3(input.x,0,input.y);
         sphereRigidBody.AddForce(inputXZPlane *  ballSpeed);
     }
+    
 }
